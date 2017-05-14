106fb7aa140e309703d2f5c127b10417d8b06b4a
caroline <caroline.chaudey.sio@gmail.com>
2017-05-14 17:42:39 +0200

    ajout relations

--- models/index.js
+++ models/index.js
@@ -5,8 +5,8 @@ const path = './entities/';
 
 module.exports = (server) => {
 
+    // config de la connexion
     server.connection = new Sequelize('service', 'postgres', 'azerty', {
-      //host: '192.168.0.33',
       host: 'localhost',
       port: 5432,
       dialect: 'postgres',
@@ -32,13 +32,53 @@ module.exports = (server) => {
     }
     console.log('Models created.');
 
-    /*
-    require('./relations.js')(server);
-    console.log('Relations between models set.');
-*/
+    // relations entre les entités (donc les tables)
+
+    // un chercheur a un superviseur
+    server.models.Seeker.belongsTo(server.models.Supervisor);
+    server.models.Supervisor.hasMany(server.models.Seeker);
+    // un chercheur a plusieurs modeles de lettres ...
+    server.models.Letter.belongsTo(server.models.Seeker);
+    server.models.Seeker.hasMany(server.models.Letter);
+    // ... et plusieurs CV
+    server.models.Resume.belongsTo(server.models.Seeker);
+    server.models.Seeker.hasMany(server.models.Resume);
+    // un chercheur peut déposer plusieurs candidatures qui lui sont propres
+    server.models.Application.belongsTo(server.models.Seeker);
+    server.models.Seeker.hasMany(server.models.Application);
+    // la candidature comprend une modèle de lettre utilisé ...
+    server.models.Application.belongsTo(server.models.Letter);
+    server.models.Letter.hasMany(server.models.Application);
+    // ... et un CV
+    server.models.Application.belongsTo(server.models.Resume);
+    server.models.Resume.hasMany(server.models.Application);
+    // la candidature est dans un état
+    server.models.Application.belongsTo(server.models.ApplicationState);
+    server.models.ApplicationState.hasMany(server.models.Application);
+    // la candidature concerne une annonce
+    server.models.Application.belongsTo(server.models.Ad);
+    server.models.Ad.hasMany(server.models.Application);
+    // L'annonce provient d'une entreprise
+    server.models.Ad.belongsTo(server.models.Company);
+    server.models.Company.hasMany(server.models.Ad);
+    // L'annonce a été postée sur un site
+    server.models.Ad.belongsTo(server.models.Website);
+    server.models.Website.hasMany(server.models.Ad);
+    // une annonce a plusieurs tags
+    server.models.Ad.belongsToMany(server.models.Tag);
+    server.models.Tag.belongsToMany(server.models.Ad);
+    // un chercheur a des sites web préférés
+    server.models.Seeker.belongsToMany(server.models.Website);
+    server.models.Website.belongsToMany(server.models.User);
+    // un seeker a des contrats préférés
+    server.models.Seeker.belongsToMany(server.models.Contract);
+    server.models.Contract.belongsToMany(server.models.Seeker);
+    // une annonce peurt proposer jusqu'à plusieurs types de contrats
+    server.models.Ad.belongsToMany(server.models.Contract);
+    server.models.Contract.belongsToMany(server.models.Ad);
+
+
     server.connection.sync().then(function() {
       console.log('Server sync successfuly');
-    //}).catch((error) => {
-      //console.log(error);
     });
 };
