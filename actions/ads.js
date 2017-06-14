983abeb90ae12585cfe54610fbc81bfcbc24cf7e
Caroline Chaudey <caroline.chaudey.sio@gmail.com>
2017-06-14 20:47:24 +0200

    ajout type contrats

--- actions/ads.js
+++ actions/ads.js
@@ -2,23 +2,57 @@
 module.exports = (api) => {
   const Ad = api.models.Ad;
   const Company = api.models.Company;
+  const ContractType = api.models.ContractType;
 
   function create(req, res, next) {
-    Company.findOrCreate({
-      where: {name: req.body.company},
-      defaults: {name: req.body.company}
+    let contractTypes = [];
+    let company;
+
+    // verifier les types de contrats
+    verifyContractTypes(req, res)
+    .then((verifiedContractTypes) => {
+      if (verifiedContractTypes.length === 0) {
+        return res.status(400).send('No valid contact types');
+      }
+      contractTypes = verifiedContractTypes;
     })
-    .spread((company, created) => {
-      Ad.create(req.body)
-      .then((ad) => {
-        if (!ad) {
-          return res.status(500).send('Couldn\'t save ad.');
-        }
-        ad.setCompany(company);
-        return res.status(200).send(ad);
+    // trouver ou créer l'entreprise concernee
+    .then(() => {
+      return Company.findOrCreate({
+        where: {name: req.body.company},
+        defaults: {name: req.body.company}
+      }).spread((givenCompany, created) => {
+        company = givenCompany;
       });
+    })
+    // créer l'annonce avec ses relations
+    .then((result) => {
+      return Ad.create(req.body);
+    })
+    .then((ad) => {
+      return ad.setCompany(company);
+    })
+    .then((adWithCompany) => {
+      console.log(adWithCompany);
+      return adWithCompany.setContractTypes(contractTypes);
+    })
+    .then((result) => {
+      return res.status(200).send(result);
     });
-  };
+ }
+
+ function verifyContractTypes(req, res) {
+    let contractTypes = req.body.contractTypes;
+    if (contractTypes.length === 0) {
+      return res.status(400).send('No contract type.');
+    }
+    return ContractType.findAll({
+      where: {
+        name: {
+          $in: contractTypes
+      }}
+    });
+  }
 
   return {create};
 }
--- routes/ads.js
+++ routes/ads.js
@@ -5,7 +5,7 @@ module.exports = (api) => {
   router.post('/',
               api.middlewares.bodyParser.json(),
               //api.middlewares.checkSupervisor,
-              api.middlewares.checkFields(['position', 'publicationDate']),
+              api.middlewares.checkFields(['position', 'publicationDate', 'contractTypes']),
               api.actions.ads.create);
 
   return router;
