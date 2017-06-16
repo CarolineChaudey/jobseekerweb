bae0ccb0573d4b3dfc3927db22f4af5549ca9644
Caroline Chaudey <caroline.chaudey.sio@gmail.com>
2017-06-16 11:48:27 +0200

    checkUser factorisation

---
+++ middlewares/checkUser.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (api) => {
+
+  return (userType) => {
+    return (req, res, next) => {
+      let authorization = req.headers['authorization'];
+      if (!authorization) {
+        return res.status(401).send('unauthorized: token required');
+      }
+      jwt.verify(
+        authorization,
+        api.settings.salt,
+        (err, decryptedToken) => { // est-ce qu'il est valide ?
+          if (err) {
+            return res.status(400).send('Invalid token.');
+          }
+          userType.findOne({token: authorization}) // qui est son user ?
+          .then((user) => {
+            if (!user) {
+              return res.status(400).send('No ' + userType + ' for this token.');
+            }
+            next();
+          });
+     });
+    }
+  }
+};
