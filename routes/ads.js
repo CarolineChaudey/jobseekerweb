d78270541fe4a647d03897f093310a0ac815facd
Caroline Chaudey <caroline.chaudey.sio@gmail.com>
2017-06-16 11:32:13 +0200

    done

--- routes/ads.js
+++ routes/ads.js
@@ -4,7 +4,7 @@ module.exports = (api) => {
 
   router.post('/',
               api.middlewares.bodyParser.json(),
-              //api.middlewares.checkSupervisor,
+              api.middlewares.checkSupervisor,
               api.middlewares.checkFields(['position', 'publicationDate', 'contractTypes', 'tags']),
               api.actions.ads.create);
 
